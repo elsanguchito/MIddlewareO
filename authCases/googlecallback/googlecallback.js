bfb0c3dbd7f62f091ae1ec876cce539240e6538f
OlverArce01 <olver.arce.limache@alumnos.uta.cl>
2023-07-16 15:22:09 -0400

    update google callback

--- authCases/googlecallback/googlecallback.js
+++ authCases/googlecallback/googlecallback.js
@@ -1,15 +1,16 @@
 import supabase from "../../database/supabase.js";
 
 const googlecallback = async (req, res) =>{
-    const accessToken = req.originalUrl.split("#access_token=")[1];
-    const queryParams = new URLSearchParams(accessToken);
+
+    const fragment = window.location.hash.substr(1); // Obtén el fragmento de la URL después del símbolo '#'
+    const queryParams = new URLSearchParams(fragment);
+    
     const accessTokenValue = queryParams.get("access_token");
     const expiresIn = queryParams.get("expires_in");
     const refreshToken = queryParams.get("refresh_token");
     const providerToken = queryParams.get("provider_token");
 
 res.json({
-    accessToken,
     queryParams,
     accessTokenValue,
     expiresIn,
